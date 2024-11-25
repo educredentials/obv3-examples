56f37a645ba9f4239e851f22444b02cf17e8e9bd
Bèr Kessels <ber@berk.es>
2024-11-25 22:00:27 +0100

    feat: Add initial offers support
    This adds offers from agent on poc4. The offers are included in a
    markdown with liquid. Lets see if jekyll builds it.

---
+++ create_offer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e # fail if any command fails, -o errexit
+set -o pipefail # fail if any command in a pipeline fails
+set -u # fail if variable is not set, -o nounset
+
+if [ "${DEBUG:-false}" = "true" ]; then
+  set -x # print commands
+fi
+
+for file in "$@"; do
+  hash=$(sha256sum "$file" | awk '{print $1}')
+  jq -c --arg preAuthorizedCode "$hash" \
+     '{credentials: ["OpenBadgeCredential"], grants: {"urn:ietf:params:oauth:grant-type:pre-authorized_code":{"pre-authorized_code":$preAuthorizedCode,"user_pin_required":false}}, credentialDataSupplierInput: .}' < "$file" | \
+
+  offer_json=$(basename "$file" .json).offer.json
+  offer_png=$(basename "$file" .json).offer.png
+  
+  curl -X POST https://agent.poc4.eduwallet.nl/edubadges/api/create-offer \
+       --silent \
+       -H "Content-Type: application/json" \
+       -H "Authorization: Bearer ${AGENT_AUTHORIZATION_CODE}" \
+       -d @- \
+       -o "./offers/${offer_json}"
+
+  uri=$(jq '.uri' -r < "./offers/${offer_json}")
+  qrcode ${uri} > "./offers/${offer_png}"
+
+  echo "<!-- managed_by_create_offer -->"
+  echo "![${offer_png}](${offer_png})"
+  echo "<!-- /managed_by_create_offer -->"
+done
---
+++ offers/index.md
@@ -0,0 +1,8 @@
+---
+title: "Offers"
+---
+
+{% assign offer_images = site.static_files | where "offer", true %}
+{% for offer in offer_images %}
+  ![{{offer}}](offer.path)
+{% endfor %}
