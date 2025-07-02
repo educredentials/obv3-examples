80bfdbc3ef423f99595ee68015e470fc0ec7ef97
Bèr Kessels <ber@berk.es>
2025-07-02 15:18:13 +0200

    feat: move to impierce agent instead of the poc4 outdated veramo-agent

--- create_offer.sh
+++ create_offer.sh
@@ -6,6 +6,10 @@ set -e # fail if any command fails, -o errexit
 set -o pipefail # fail if any command in a pipeline fails
 set -u # fail if variable is not set, -o nounset
 
+_AGENT_CREATE_CREDENTIALS_URL="https://agent.poc9.eduwallet.nl/v0/credentials"
+_AGENT_CREATE_OFFER_URL="https://agent.poc9.eduwallet.nl/v0/offers"
+
+
 if [ "${DEBUG:-false}" = "true" ]; then
   set -x # print commands
 fi
@@ -44,21 +48,36 @@ for file in "$@"; do
   offer_name=$(jq -r '.credentialSubject.achievement.name' < "$file")
   offer_issuer=$(jq -r '.issuer.name' < "$file")
 
+  offer_txt_file="./offers/txt/$(basename "$file" .json).offer.txt"
   offer_json_file="./offers/txt/$(basename "$file" .json).offer.json"
   offer_png_file="./offers/img/$(basename "$file" .json).offer.png"
 
+  # Create the credential on the agent with a unique offerId
   jq -c --arg id "$hash" \
-     '{credentials: ["OpenBadgeCredential"], grants: {"urn:ietf:params:oauth:grant-type:pre-authorized_code":{"pre-authorized_code":$id,"user_pin_required":false}}, credentialDataSupplierInput: { offerId:$id, credentialConfigurationId:"OpenBadgeCredential", credential:.}}' < "$file" | \
-
-  curl -X POST https://agent.poc4.eduwallet.nl/edubadges/api/create-offer \
+     '{offerId:$id, credentialConfigurationId:"openbadge_credential", expiresAt:"never", isSigned:false, credential:.}' < "$file" | \
+  curl -X POST "${_AGENT_CREATE_CREDENTIALS_URL}" \
        --silent \
        -H "Content-Type: application/json" \
        -H "Authorization: Bearer ${AGENT_AUTHORIZATION_CODE}" \
        -d @- \
-       -o "${offer_json_file}"
+       > /dev/null # no output, the response is merely a report of success
 
-  uri=$(jq '.uri' -r < "${offer_json_file}")
+  # Create the offer on the agent with the same offerId
+  curl -X POST "${_AGENT_CREATE_OFFER_URL}" \
+       --silent \
+       -H "Content-Type: application/json" \
+       -H "Authorization: Bearer ${AGENT_AUTHORIZATION_CODE}" \
+       -d "{\"offerId\": \"$hash\", \"preAuthorizedCode\": \"$hash\" }" \
+       -o "${offer_txt_file}"
+
+  uri=$(<"${offer_txt_file}")
   qrcode ${uri} > "${offer_png_file}"
+
+  # Build a json file with the offer details, a uri, and id.
+  jq -n \
+    --arg id "$hash" \
+    --arg uri "$uri" \
+    '{id: $id, uri: $uri}' > "${offer_json_file}"
 
   add_item \
     "$file" \
